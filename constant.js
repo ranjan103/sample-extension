94e04df86688ba5e1b7ba761ef4fa5a8f902c721
ranjan103 <abhishek.171co203@nitk.edu.in>
2023-01-13 16:17:15 +0530

    First Commit

---
+++ constant.js
@@ -0,0 +1,7 @@
+const constants = {
+  location: "New+York+City",
+  max_page_count: 1,
+  url: "https://www.yelp.com/search?find_desc=Italian+Restaurants&find_loc=${location}&start=${pageCounter}",
+};
+
+module.exports = { constants };
---
+++ src/fetch.js
@@ -0,0 +1,61 @@
+const puppeteer = require("puppeteer");
+const { JSDOM } = require("jsdom");
+const fs = require("fs");
+const { constants } = require("../constant");
+const { window } = new JSDOM();
+
+async function scrapeYelp()  {
+  const location = document.getElementById('location').value;
+  // Record start time for performance analysis
+  const start = window.performance.now();
+
+  // Launch a new browser instance
+  const browser = await puppeteer.launch();
+
+  // Create a new page
+  const page = await browser.newPage();
+  let pageCounter = 1;
+
+  // Initialize array to store extracted data
+  let restaurants = [];
+
+  // Scrape data while pageCounter is less than or equal to the max number of pages
+  while (pageCounter <= constants.max_page_count) {
+    // Construct the URL for searching for Italian restaurants in the given location
+    let url = constants.url
+      .replace("${location}", location)
+      .replace("${pageCounter}", pageCounter);
+
+    // Navigate to the URL
+    await page.goto(url, { waitUntil: "load", timeout: 0 });
+
+    // Extract the text content of elements that match the CSS selector
+    const newRestaurants = await page.$$eval(".css-1agk4wl", (elements) =>
+      elements.map((el) => el.innerText)
+    );
+    restaurants = [...restaurants, ...newRestaurants];
+
+    for (const restaurant of newRestaurants) {
+      fs.appendFile(
+        "./../result.csv",
+        `${restaurant.replace(/^[0-9]+/, "").replace(". ", "").replace(",", "\n")}\n`,
+        function (err) {
+          if (err) {console.log(err); throw err;}
+        }
+      );
+    }
+    pageCounter++;
+  }
+
+  // Close the browser
+  await browser.close();
+
+  // Record end time for performance analysis
+  const end = window.performance.now();
+  console.log(`Execution time: ${end - start} ms`);
+
+  // Return the extracted data
+  return restaurants;
+};
+
+// module.exports = { scrapeYelp };
